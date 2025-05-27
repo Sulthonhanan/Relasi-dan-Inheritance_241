8126b751634638d70717dd8bfb9c2ef786dde64e
Sulthonhanan <hananakhnaf31@gmail.com>
2025-05-27 11:26:13 +0700

    Implementasi pewarisan class pelajar dari orang dengan metode perkenalan

--- Inheritance/pewarisan.cpp
+++ Inheritance/pewarisan.cpp
@@ -23,3 +23,30 @@ public:
         return a + b;
     }
 };
+
+class pelajar : public orang
+{
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah)
+    {
+        cout << "pelajar dibuat\"" << endl;
+    }
+    ~pelajar()
+    {
+        cout << "pelajar dihapus\n"
+             << endl;
+    }
+    string perkenalan(){
+        return "Halo, nama saya " + nama + " dari sekolah" + sekolah + "\n\n"}
+};
+
+int main()
+{
+    pelajar siswa1("andi laksono", "sman 1 bantul");
+    cout << siswa1.perkenalan();
+    cout << "Hasil = " << siswa1.jumlah(10, 99) << endl;
+
+    return 0;
+}
