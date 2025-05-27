d8235b0817f102ae799f2c8d4f0def2c5c993fe5
Sulthonhanan <hananakhnaf31@gmail.com>
2025-05-27 11:25:40 +0700

    Implementasi awal kelas orang dengan fungsi penjumlahan

---
+++ Inheritance/pewarisan.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class orang
+{
+public:
+    string nama;
+
+    orang(string pNama) : nama(pNama)
+    {
+        cout << "oranag dibuat\n"
+             << endl;
+    }
+    ~orang()
+    {
+        cout << "orang dihapus\n"
+             << endl;
+    }
+
+    int jumlah(int a, int b)
+    {
+        return a + b;
+    }
+};
